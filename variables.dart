5681d7413b05e78876b370eed5e56ce241e07e01
sairamBR <sairam.au18@bitsathy.ac.in>
2021-02-05 11:15:19 +0530

    variables in dart

---
+++ variables.dart
@@ -0,0 +1,17 @@
+/// in dart we don't need to declear the type for the variable it automatical detect the type
+var name = "sairam";
+var dept = " BE Automobile Engineering";
+var acadamic = " 3rd Year";
+var collageName = " Bannari Amman Institute of technology ";
+var linebreak = "\n";
+void main() {
+  print(name +
+      '\n' +
+      dept +
+      '\n' +
+      acadamic +
+      '\n' +
+      collageName +
+      '\n' +
+      linebreak);
+}
